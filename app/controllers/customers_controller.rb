d143279d3ef0fe17791ca33a29ee64c1c7d56dee
patrick.jiang <patrick.jiang86@gmail.com>
2020-03-20 19:21:05 -0500

    Added CSS to Customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def alphabetized
+    @customers = Customer.order('fullname ASC')
   end
 
   def missing_email
+    @customers = Customer.where(email: [nil, ''])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'customers/index'
+  root to: 'customers#index'
   get 'customers/alphabetized'
   get 'customers/missing_email'
   devise_for :admin_users, ActiveAdmin::Devise.config
